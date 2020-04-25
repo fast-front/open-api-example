60ecf1cd07b9a33868bba2831a26220d63104732
ryoichikishimoto <rkishimoto@fastfront.biz>
2020-04-25 20:55:01 +0900

    feat: 環境整備、おすすめ一覧定義

--- scripts/open-api-generator.js
+++ scripts/open-api-generator.js
@@ -5,18 +5,18 @@ const inputYaml = process.env.INPUT || 'shoken-owner-app.v1.yaml'
 
 const langMap = {
   ts: 'typescript-axios',
-  dart: 'dart',
+  dart: 'dart -DbrowserClient=false',
   php: 'php-laravel',
 }
 
 const outDirMap = {
     php: `${__dirname}/generated-sources/server/owner`,
-    dart: process.env.OUT_DIR_OWNER_DART,
-    ts: process.env.OUT_DIR_MANAGEMENT_TS,
+    dart: process.env.OUT_DIR_DART,
+    ts: process.env.OUT_DIR_TS,
 }
 
 const api = inputYaml
 const lang = langMap[process.env.LANG]
-const outputDir = outDirMap[process.env.API][process.env.LANG]
+const outputDir = outDirMap[process.env.LANG]
 
 shell.exec(`openapi-generator generate -i ${api} -g ${lang} -o ${outputDir}`)
